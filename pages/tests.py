cc5c129419fccedb19720a2a9a5b8cefc8b1327a
code2319 <{ID}+{username}@users.noreply.github.com>
2022-12-12 21:52:13 +0300

    initial commit

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        r = self.client.get("/")
+        self.assertEqual(r.status_code, 200)
+
+    def test_url_available_by_name(self):
+        r = self.client.get(reverse("home"))
+        self.assertEqual(r.status_code, 200)
+
+    def test_template_available_by_name(self):
+        r = self.client.get(reverse("home"))
+        self.assertTemplateUsed(r, "home.html")
+
+    def test_template_content(self):
+        r = self.client.get(reverse("home"))
+        self.assertContains(r, "<h1>Homepage</h1>")
+
+
+class AboutpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        r = self.client.get("/about/")
+        self.assertEqual(r.status_code, 200)
+
+    def test_url_available_by_name(self):
+        r = self.client.get(reverse("about"))
+        self.assertEqual(r.status_code, 200)
+
+    def test_template_available_by_name(self):
+        r = self.client.get(reverse("about"))
+        self.assertTemplateUsed(r, "about.html")
+
+    def test_template_content(self):
+        r = self.client.get(reverse("about"))
+        self.assertContains(r, "<h1>About page</h1>")
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse("Hellow, World!")
+class HomePageView(TemplateView):
+    template_name: str = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name: str = "about.html"
